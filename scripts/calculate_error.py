5887d317272831a41ea221680025143b09d8d2ff
Francisca Oliveira e Silva <franciscaoliveiraesilva@gmail.com>
2023-09-29 20:42:59 +0100

    Plotting elipse and other important points at program exit

--- scripts/calculate_error.py
+++ scripts/calculate_error.py
@@ -38,6 +38,8 @@ gt_frame = None
 est_pose_data = []
 ellipse_data = []
 gt_pose_data = []
+lower_bound_pose_data = []
+upper_bound_pose_data = []
 
 
 # def extrema_to_ellipse(gt_tf: TransformStamped, uncertainty_ellipse: tuple):
@@ -58,6 +60,8 @@ def extrema_to_ellipse(gt_tf: TransformStamped, uncertainty_ellipse: tuple):
     # Generate points on the ellipse before rotation
     theta = np.linspace(0, 2 * np.pi, 1000)
 
+    global x_rot, y_rot
+    
     # Rotate the ellipse alpha degrees counterclockwise
     x_rot = (a*np.cos(theta)*np.cos(alpha)) - (b*np.sin(theta)*np.sin(alpha)) + x0 
     y_rot = (a*np.cos(theta)*np.sin(alpha)) + (b*np.sin(theta)*np.cos(alpha)) + y0
@@ -91,6 +95,9 @@ def calculate_error(gt_tf: TransformStamped, pose: PoseStamped):
 
 def calculate_bounded_error(gt_tf, pose, uncertainty_ellipse, bf2bs_tf):
     (lower_bound_pose, higher_bound_pose) = extrema_to_ellipse(gt_tf, uncertainty_ellipse)
+
+    lower_bound_pose_data.append([lower_bound_pose.pose.position.x, lower_bound_pose.pose.position.y])
+    upper_bound_pose_data.append([higher_bound_pose.pose.position.x, higher_bound_pose.pose.position.y])
 
     pose = tf2_geometry_msgs.do_transform_pose(pose, bf2bs_tf)
     lower_bound_pose = tf2_geometry_msgs.do_transform_pose(lower_bound_pose, bf2bs_tf)
@@ -132,12 +139,12 @@ def odometry_callback(data: Odometry):
     else:
         time_data.append(curr_time-initial_time)
         error_data.append(bounded_error)
-        # est_pose_data.append([pose.pose.position.x, pose.pose.position.y])
-        # gt_pose_data.append([gt_tf.transform.translation.x, gt_tf.transform.translation.y])
+        est_pose_data.append([pose.pose.position.x, pose.pose.position.y])
+        gt_pose_data.append([gt_tf.transform.translation.x, gt_tf.transform.translation.y])
 
 
 def main():
-    global tf_buffer, gt_frame, initial_time, time_data, error_data, est_pose_data, gt_pose_data, ellipse_data
+    global tf_buffer, gt_frame, initial_time, time_data, error_data, est_pose_data, gt_pose_data, ellipse_data, x_rot, y_rot
     parser = argparse.ArgumentParser()
     parser.add_argument('--gt_frame', help='The child frame of the GT transform', default='mocap_laser_link')
     parser.add_argument('--est_topic', help='The odometry topic of the estimation', default='/odometry/filtered')
@@ -174,6 +181,23 @@ def main():
     # plt.plot([est_pose_data[10][0]], [est_pose_data[10][1]])
     # plt.plot([gt_pose_data[10][0]] , [gt_pose_data[10][1]])
     # plt.show() 
+
+    plt.title("Elipse at time %.2f seconds" % time_data[-1])
+    plt.ylabel("y coordinate (m)")
+    plt.xlabel("x coordinate (m)")
+    plt.axis('equal')
+    plt.plot(x_rot, y_rot, label='Covariance ellipse')
+    print("\n Timestamp: ", time_data[-1] + initial_time)
+    print("\n Ground thruth: ", est_pose_data[-1][0], est_pose_data[-1][1])
+    plt.plot(gt_pose_data[-1][0] , [gt_pose_data[-1][1]], 'ro', label='Ground truth')
+    print("\n Estimated ", est_pose_data[-1][0] , est_pose_data[-1][1])
+    plt.plot(est_pose_data[-1][0], [est_pose_data[-1][1]], 'go', label='Estimated position')
+    print("\n Lower bound: ", error_data[-1][0])
+    plt.plot(lower_bound_pose_data[-1][0], [lower_bound_pose_data[-1][1]], 'yx', label='Lower bound')
+    print("\n Upper bound: ", error_data[-1][2])
+    plt.plot(upper_bound_pose_data[-1][0], [upper_bound_pose_data[-1][1]], 'cx', label='Upper bound')
+    plt.legend(loc='best')
+    plt.show() 
 
     plt.title("Mocap vs Estimation Error")
     plt.ylabel("Distance (mm)")
